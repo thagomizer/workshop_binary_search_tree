a1568d00408a83e6c2a1133829db38ba722ebffd
Scott Windsor <swindsor@gmail.com>
2017-01-03 20:02:32 -0800

    Le pass!

--- binary_tree.rb
+++ binary_tree.rb
@@ -4,10 +4,16 @@ class BinaryTree
   end
 
   def insert *args
-    @tree += args
+    args.each do |a|
+      @tree << a
+    end
   end
 
   def include? *args
     args.all? { |x| @tree.include?(x) }
+  end
+
+  def height
+    (@tree.length / 2) + (@tree.length % 2)
   end
 end
