f89d92a9411df11c3527012dc9bce115d10f7177
Aja Hammerly <aja.hammerly@gmail.com>
2017-01-03 19:54:42 -0800

    POING!

--- test_binary_tree.rb
+++ test_binary_tree.rb
@@ -15,4 +15,21 @@ class BinaryTreeTest < Minitest::Test
     assert bt.include?(1)
     refute bt.include?(21321)
   end
+
+  def test_it_can_has_height
+    bt = BinaryTree.new
+    bt.insert 2
+
+    assert_equal 1, bt.height
+
+    bt.insert 1
+    bt.insert 3
+
+    assert_equal 2, bt.height
+
+    bt.insert 4
+    bt.insert 5
+
+    assert_equal 3, bt.height
+  end
 end
